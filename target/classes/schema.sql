45029fc15b21e5b981b579a5f7d702c2692f7dcd
hulingfengabc <13185089160@163.com>
2025-05-18 09:45:59 +0800

    rich read me

--- target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE transaction IF EXISTS;
-
-CREATE TABLE transaction (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    type VARCHAR(20) NOT NULL,
-    amount DECIMAL(15,2) NOT NULL CHECK (amount >= 0),
-    description VARCHAR(255),
-    date TIMESTAMP NOT NULL
-);
